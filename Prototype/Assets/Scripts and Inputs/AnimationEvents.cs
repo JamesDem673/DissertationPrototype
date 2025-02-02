1eae8acca3d606dc076aa0e08daa6c99ea91308a
JamesDem673 <james2.dempsey@live.uwe.ac.uk>
2025-02-02 16:07:47 +0000

    Fixed Combat

--- Prototype/Assets/Scripts and Inputs/AnimationEvents.cs
+++ Prototype/Assets/Scripts and Inputs/AnimationEvents.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Xml.Serialization;
 using UnityEngine;
 
@@ -8,6 +9,9 @@ public class AnimationEvents : MonoBehaviour
     [SerializeField] Animator _animator;
    
     [SerializeField] public GameObject _player;
+    public GameObject swingOne;
+    public GameObject swingTwo;
+
     private Movement _pm;
 
     public void Start()
@@ -29,5 +33,29 @@ public class AnimationEvents : MonoBehaviour
     {
         _pm._nextAttack = false;
         _animator.SetBool("NextAttack", false);
+    }
+
+    public void ToggleSwingOne()
+    {
+        if(swingOne.activeInHierarchy)
+        {
+            swingOne.SetActive(false);
+        }
+        else
+        {
+            swingOne.SetActive(true);
+        }
+    }
+
+    public void ToggleSwingTwo()
+    {
+        if (swingTwo.activeInHierarchy)
+        {
+            swingTwo.SetActive(false);
+        }
+        else
+        {
+            swingTwo.SetActive(true);
+        }
     }
 }
